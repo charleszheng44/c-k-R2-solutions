dfabf94d0cbc0d29e3c950a2599959451c385a2c
Charles Zheng <charleszheng44@gmail.com>
2017-02-02 13:18:14 -0500

    update 1_22 and 1_23

--- chp1/1_22.c
+++ chp1/1_22.c
@@ -5,3 +5,101 @@
  * very long lines, and if there are no blanks or tabs before the specified 
  * column.
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#define MAXLINE     1024
+#define LEN_LIMIT   30
+
+int cut(char *line, int len, int limit);
+int get_line(char *line, int size);
+
+int main(void) 
+{
+    char line[MAXLINE];
+    int lin_len;
+    int offset;
+    int i;
+   
+    offset = 0; 
+
+    while((line_len = get_line(line, MAXLINE)) != -1) {
+        if(line_len <= LEN_LIMIT) {
+            printf("%d", line);
+        } else {
+            while(line + LEN_LIMIT <= len) {
+                offset = cut(line, line_len, LEN_LIMIT);
+                for(i = 0; i <= offset; i++) {
+                    putchar(line[i]);
+                }
+                putchar('\n');
+                line += offset; 
+            }  
+            
+            printf("%s\n", line);
+        } 
+    }
+}
+
+int cut(char *line, int len, int limit)
+{
+    char c;
+    int i;
+    int end_wrd_pos;
+    int in_wrd;
+
+    i = 0;
+    end_wrd_pos = 0;
+    in_wrd = 0;
+    
+    for(; i < len; i++) {
+        if(i == limit) {
+            if( end_wrd_pos == 0) {
+                fprintf(stderr, "Word length exceed the limit\n");
+            } else {
+                return end_wrd_pos;
+            }
+        }
+
+        if(line[i] == ' ' || line[i] == '\t') {
+            if(in_wrd) {
+                end_wrd_pos = i-1;
+            }
+        } else {
+            if(!in_wrd) {
+                in_wrd = 1;
+            }
+        }
+
+    } 
+    
+}
+
+int get_line(char *line, int size)
+{
+    char c;
+    int i;
+
+    i = 0;
+   
+    while((c = getchar()) != EOF && c != '\n' && i < size) {
+        line[i++] = c;
+    }
+    
+    if(i >= size) {
+        fprintf(stderr, "Line length exceed the limit");
+        exit(EXIT_FAILURE);
+    }
+
+    if(c == '\n') {
+        line[i++] = c;
+    } 
+
+    line[i++] = '\0';
+    
+    if(c == EOF) {
+        i = -1
+    }
+
+    return i;
+}
--- chp1/1_23.c
+++ chp1/1_23.c
@@ -3,9 +3,12 @@
  * forget to handle quoted strings and character constants properly. 
  * C comments do not nest.
  */
+
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+void remove_comment(const char *c_fn);
 
 int main(int argc, char *argv[])
 {
@@ -17,6 +20,7 @@ int main(int argc, char *argv[])
     while((opt = getopt(argc, argv, "f:h")) != -1) {
         if(opt == 'f') {
             inp_fn = optarg;
+            remove_comment(inp_fn);
         } else if( opt == 'h') {
             printf("\nuse option -f to specify a c source file\n\n");
         }
@@ -32,27 +36,47 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-void remove_comment(const char* c_fn) 
+void remove_comment(const char *c_fn) 
 {
     FILE *fp;
     char *line = NULL;
     size_t len = 0;
     ssize_t read;
+    int block_comment = 0;
 
     fp = fopen(c_fn, "r");
     while((read = getline(&line, &len, fp)) != -1) {
-        int i;
-        for(i = 0; i < read; i++) {
-            //remove the heading blanks
-            if(line[i] != ' ' && line[i] != '\t') {
-                break;
-            }
-        } 
+        int i = 0;
+        if(block_comment) {
+            for(; i < read; i++) {
+                //remove the heading blanks
+                if(line[i] != ' ' && line[i] != '\t') {
+                    break;
+                }
+            } 
 
-        if(i >= read || line[i] == '/') {
-            continue;
-        } else {
-            printf("%s", line);
+            if(i >= read) {
+                continue;
+            }
+        }
+
+        for(; i < read-1; i++) {
+            if(line[i] == '/' && line[i+1] == '/') {
+                break;
+            } else if(line[i] == '/' && line[i+1] == '*') {
+                block_comment = 1;
+            } else if(line[i] == '*' && line[i+1] == '/') {
+                block_comment = 0;
+                i++;
+            } else {
+                if(!block_comment) {
+                    putchar(line[i]); 
+                }
+            }
+        }
+        
+        if(!block_comment) {
+            putchar(line[read-1]);
         }
 
     }
