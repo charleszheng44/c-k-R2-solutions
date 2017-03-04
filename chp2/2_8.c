590dc1445f90cdc7419f61b42a32f9a1ea213225
Chao Zheng <charleszheng44@gmail.com>
2017-03-04 12:20:19 -0500

    update 2_8

--- chp2/2_8.c
+++ chp2/2_8.c
@@ -9,6 +9,7 @@
 
 #define STR_LEN 1024
 
+const char *integer_to_binary_str(unsigned int n);
 unsigned int rightrot(unsigned int x, unsigned int n);
 
 int main(void)
@@ -16,14 +17,25 @@ int main(void)
     unsigned int a = 156;
     unsigned int b = rightrot(156, 3);
 
-    printf("%u -> %u\n", a, b);
+    printf("%u : %s -> %u : %s \n", a, integer_to_binary_str(a), b, integer_to_binary_str(b));
     return 0;
 }
 
 unsigned int rightrot(unsigned int x, unsigned int n)
 {
-    size_t len = sizeof(x) * CHAR_BIT;
-    size_t rot;
+
+    unsigned int y = x;
+    unsigned int len = 0;
+    size_t s = sizeof(x) * CHAR_BIT;
+
+    while(y >= 1) {
+        y = y / 2;
+        len ++;     
+    }
+    
+    printf("num of bits: %u\n", len);
+
+    unsigned int rot;
 
     if(n >= len) {
         rot = n % len;
@@ -31,9 +43,38 @@ unsigned int rightrot(unsigned int x, unsigned int n)
         rot = n;
     }
 
+    printf("rotate of bits: %u\n", rot);
     if((x == 0) || (rot == 0)) {
         return x;
     }
+   
+    unsigned int a = x >> rot;
+    unsigned int b = (x & (~(~0U << rot))) << (len - rot);
+     
+    printf("num a: %u binary format: %s\n", a, integer_to_binary_str(a));
+    printf("num b: %u binary format: %s\n", b, integer_to_binary_str(b));
 
-    return (x >> rot) | (x << (len - rot));
+    return (a | b);
+}
+
+const char *integer_to_binary_str(unsigned int n)
+{
+    char buff[STR_LEN];
+    char reminder;
+    int j = 0;
+    while(n >= 1) {
+        
+        reminder = (n % 2 == 1 ? '1' : '0');
+        buff[j++] = reminder; 
+
+        n = n / 2; 
+    } 
+
+    char *bin_str = malloc(j * sizeof(char));
+    int i;
+    for(i = 0, j = j-1; j >= 0; j--, i++) {
+        bin_str[i] = buff[j];
+    }
+   
+    return (const char *)bin_str; 
 }
