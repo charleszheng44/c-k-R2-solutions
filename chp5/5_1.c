a79ee574db8e70e694010bedd85372020325e8d9
Chao Zheng <charleszheng44@gmail.com>
2017-06-08 22:10:55 -0700

    update 5_2

--- chp5/5_1.c
+++ chp5/5_1.c
@@ -1,3 +1,10 @@
+
+/*
+ * As written, getint treats a + or - not followed by a digit as a valid 
+ * representation of zero. Fix it to push such a character back on the 
+ * input.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
--- chp5/5_2.c
+++ chp5/5_2.c
@@ -8,6 +8,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 #define BUF_SIZE    64
 
@@ -33,17 +34,66 @@ void ungetch(int c)
 
 }
 
-int getfloat(float *) 
+int getfloat(float *pn) 
 {
     int c;
+    int tmp = 0;
+    int sign; 
 
     while(isspace(c = getch()))
         ;
 
-    if(!isdigit(c) && c != EOF && c != "+" && c != "-" && c != ".") {
+    if(!isdigit(c) && c != EOF && c != '+' && c != '-' && c != '.') {
         ungetch(c);
         return 0;
     }
+
+    if(c == '.') {
+        goto PROC_DECIMAL;
+    }
+    
+    sign = (c == '-') ? -1 : 1; 
+    
+    if(c == '-' || c == '+') {
+        tmp = c;
+        c = getch();
+    } 
+
+    if(!isdigit(c)) {
+        ungetch(c);
+        if(tmp != 0) {
+            ungetch(tmp);
+        }
+    }
+    
+    for(*pn = 0; isdigit(c); c = getch()) {
+        *pn = 10 * *pn + (c - '0');
+    }
+    
+    if(c != '.') {
+        *pn = *pn * sign;
+        return 0;
+    }
+
+PROC_DECIMAL: 
+    
+    if(!isdigit(c = getch())) {
+        ungetch(c);
+        return c;
+    } 
+    
+    float curr_digit = 0.1;
+
+    while(isdigit(c)) {
+        *pn = *pn + curr_digit * (c - '0'); 
+        curr_digit = curr_digit / 10;
+    } 
+    
+    if(c != EOF) {
+        ungetch(c);
+    } 
+
+    return c;
 }
 
 int main(void)
